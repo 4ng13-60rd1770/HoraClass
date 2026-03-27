8b10c20e0596633c61789862e46f934fb4911cae
Angie Gordillo <angielorenagordilloroa@gmail.com>
2026-03-27 02:06:32 -0500

    refactor: simplificar scripts de inicio y parada de Docker

--- stop.sh
+++ stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "🛑 Parando HoraClass..."
-docker compose down --rmi all --volumes --remove-orphans
-docker system prune -f
-echo "✅ Proyecto detenido y limpio"
+docker compose -f docker-compose.yaml down --rmi all --volumes
+echo "🛑 Detenido"
