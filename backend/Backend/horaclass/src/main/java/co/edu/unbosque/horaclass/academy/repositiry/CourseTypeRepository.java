83cf95abb2d183dd360d9b7b24137a4f89d32586
Angie Gordillo <angielorenagordilloroa@gmail.com>
2026-03-26 22:44:49 -0500

    🧹 Limpieza: solo backend/ + frontend/ + archivos raíz

--- backend/Backend/horaclass/src/main/java/co/edu/unbosque/horaclass/academy/repositiry/CourseTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.horaclass.academy.repositiry;
-
-import co.edu.unbosque.horaclass.academy.model.CourseType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseTypeRepository extends JpaRepository<CourseType, Integer> {
-}
