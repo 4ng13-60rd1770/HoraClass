9535b2f2b9eda1676e05fa6a0dc57c160efd35de
Angie Gordillo <angie.gordillor@puntored.co>
2026-05-03 18:38:48 -0500

    primer commit

---
+++ backend/src/main/java/co/edu/unbosque/horaclass/HoraclassApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.horaclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraclassApplication.class, args);
+	}
+
+}
